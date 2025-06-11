4ca850f80fc155165dc9afa62af78de70df5a8ba
Dhv213 <dimpu6969@gmail.com>
2025-06-11 14:18:39 +0530

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,15 @@
+public class Task {
+    private int id;
+    private String title;
+    private String description;
+    private String deadline;
+
+    public Task(int id, String title, String description, String deadline) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.deadline = deadline;
+    }
+
+    // Getters and Setters
+}
